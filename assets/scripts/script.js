e20340f1323af4516906ea095e0aeaeb5b2e047f
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-02 10:27:14 +0200

    modification du html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,12 @@
 let form = document.querySelector("#formulaire");
+let card = document.querySelector("recapCard");
+
+form.addEventListener('submit', (e) => {
+    //permet d'arreter la soumission du formulaire
+    e.preventDefault();
+    //le reste vient ici
+    let name = document.querySelector("#nom");
+    console.log(name.value);
+
+    
+})
